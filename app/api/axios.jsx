5308f21a65e6df3cacf9da8ef3ec65934f7972f3
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2024-05-28 09:09:27 +0100

    connected to deployed backend

--- app/api/axios.jsx
+++ app/api/axios.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+// const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://invoice-be-nnp4.onrender.com';
 
 export default axios.create({
     baseURL: BASE_URL
