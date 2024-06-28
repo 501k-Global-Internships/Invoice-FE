df91545ad330210621f62c71ff14e8f2ff5f9389
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2024-06-28 12:56:22 +0100

    fixes with reset password

--- app/account.js
+++ app/account.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from "next/link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './Account.css';
+import './account.css';
 import { faArrowRightToBracket, faKey } from '@fortawesome/free-solid-svg-icons';
 
 const Account = () => {
